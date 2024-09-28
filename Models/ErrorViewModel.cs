fa2163e9e509359857f969046fff2b0edab22ed3
Iamkunda <Iamkundasakala@gmail.com>
2024-09-28 21:00:50 +0200

    create project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PamasolaSimpleERP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
