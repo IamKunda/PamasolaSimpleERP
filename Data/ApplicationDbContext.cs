23e95f19a053c27751ca22b42db54b37177e24e0
Iamkunda <Iamkundasakala@gmail.com>
2024-09-30 07:45:21 +0200

    edit dbcontext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,12 +9,6 @@ namespace PamasolaSimpleERP.Data
             : base(options) { }
 
         // public DbSet<UserModel> Tb_Users { get; set; } = null!;
-        // // store one time pins
-        // public DbSet<UserOtp> Tb_Otps { get; set; } = null!;
-
-        // public DbSet<FoodItemModel> Tb_FoodItems { get; set; } = null!;
-        // public DbSet<RestaurantModel> Tb_Restaurants { get; set; } = null!;
-        // public DbSet<OrderModel> Tb_Orders { get; set; } = null!;
         // dotnet ef migrations add Initial
         // dotnet ef database update
     }
